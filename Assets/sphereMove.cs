409b20515a6ba4224efec06aa3b14da1eb53d6ea
saimivilkkila <saimi.vilkkila@gmail.com>
2024-03-11 13:12:11 +0200

    Assignment updated

--- Assets/sphereMove.cs
+++ Assets/sphereMove.cs
@@ -7,34 +7,32 @@ public class sphereMove : MonoBehaviour
     //just speed for the ball
     [SerializeField]
     private float speed;
-    private float radius; // radius of sphere to set the raycast to a good position
     private Vector3 direction; //direction of balls movement
     private Vector3 colVec; // Distance from ball to wall, achieved with raycast
-    private Ray ray; //our ray 
-    private RaycastHit hit; //raycast hit info
+    private bool incontact;
 
     private void Start()
     {
         direction = Vector3.forward;
-        radius = gameObject.GetComponent<SphereCollider>().radius * 0.5f;
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        colVec = col.contacts[0].normal;
+        incontact = true;
+    }
+    private void OnCollisionExit(Collision col)
+    {
+        incontact = false;
     }
 
     void Update()
     {
-        //moving the ball
-        transform.Translate(direction * Time.deltaTime);
-        //make a raycast to detect a wall, also make sure raycast starts from the EDGE of the ball, even when direction changes, so collision looks better
-        Vector3 raycastOrigin = direction * radius;
-        ray = new Ray(transform.position + raycastOrigin, direction);
-        Debug.DrawRay(ray.origin, ray.direction, Color.green);
-        //if raycasts hits something, calculate distance between ball and the hit object
-        if (Physics.Raycast(ray, out hit))
+        transform.Translate(direction * speed * Time.deltaTime);
+        if (incontact)
         {
-            colVec = hit.point - ray.origin; //distance from ball to origin
-            Debug.Log(Vector3.Dot(ray.direction.normalized, colVec.normalized));
-            //if dot product is less than 1, this means we are no longer right against the object the
-            //raycast hit, which means we are pass it, so we change the direction vector to an opposite direction
-            if (Vector3.Dot(ray.direction.normalized, colVec.normalized) < 1)
+            Debug.Log(Vector3.Dot(direction.normalized, colVec.normalized));
+            if (Vector3.Dot(direction.normalized, colVec.normalized) < 1)
             {
                 direction.x *= -1;
                 direction.y *= -1;
@@ -43,9 +41,4 @@ public class sphereMove : MonoBehaviour
         }
     }
 }
-//NOTE:
-//There is an issue with this code, because if this didn't for some reason detect the change INSTANTLY
-//the raycast would go pass the wall seen in the scene, and the raycast would not be hitting anything,
-//and the ball would fly through. Could you help me understand how to prevent this? 
-//my school email is saimisachiko.vilkkila@tuni.fi
 
